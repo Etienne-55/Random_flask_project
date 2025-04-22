aa175def357218b642b5fca8bd51a304f2662fd7
etienne <etienne.pautet@gmail.com>
2025-04-22 20:08:20 +0100

    feat: login, database conexion

---
+++ config.py
@@ -0,0 +1,4 @@
+class Config:
+    SECRET_KEY ='your-secret-key'
+    SQLALCHEMY_DATABASE_URI = 'postgresql://etienne:secure_password123@localhost/flask_db'
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
