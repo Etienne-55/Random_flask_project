8caf0548778d0276e07b51c9bec5a748b4a3010c
etienne <etienne.pautet@gmail.com>
2025-04-22 20:07:16 +0100

    feat: login, creating first routes

--- app.py
+++ app.py
@@ -1,9 +1,29 @@
 from flask import Flask
-app = Flask(__name__)
+from flask_sqlalchemy import SQLAlchemy
+from config import Config 
 
-@app.route('/')
-def hello():
-    return "Hello World from rapsberry"
+db = SQLAlchemy()
+
+def create_app():
+    app = Flask(__name__)
+    app.config.from_object(Config)
+
+    db.init_app(app)
+
+    @app.route('/')
+    def hello():
+        return "Hello World from rapsberry" 
+
+    @app.route('/first-route')
+    def first_route():
+        return "testinf route"
+
+    with app.app_context():
+        db.create_all()
+
+    return app
+
+app = create_app()
 
 if __name__ == '__main__':
     app.run (host='0.0.0.0', port=5000)
