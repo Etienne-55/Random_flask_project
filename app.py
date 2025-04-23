d259fa47a78f30ccd932eb313a3cfec5c7847d52
etienne <etienne.pautet@gmail.com>
2025-04-23 23:48:59 +0100

    feat: login, created models for login

--- app.py
+++ app.py
@@ -1,14 +1,20 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_jwt_extended import JWTManager
 from config import Config 
+from flask_migrate import Migrate
 
+migrate = Migrate()
 db = SQLAlchemy()
+jwt = JWTManager()
 
 def create_app():
     app = Flask(__name__)
     app.config.from_object(Config)
 
     db.init_app(app)
+    migrate.init_app(app,db)
+    jwt.init_app(app)
 
     @app.route('/')
     def hello():
