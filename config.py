1304f57daafd4cc35b5ad3f3777d2269ea84e6a1
etienne <etienne.pautet@gmail.com>
2025-04-29 20:25:49 +0100

    chore: config.py

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
 class Config:
     SECRET_KEY ='your-secret-key'
-    SQLALCHEMY_DATABASE_URI = 'postgresql://user:password@localhost/database'
+    SQLALCHEMY_DATABASE_URI = 'postgresql://etienne:secure_password123@localhost/flask_db'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
